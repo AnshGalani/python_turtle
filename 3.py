136cc87ae837d7df58cd07b7cfa28ec4875411b2
Ansh Galani <85383708+AnshGalani@users.noreply.github.com>
2021-06-16 10:20:10 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,18 @@
+import turtle as t
+t.bgcolor("black")
+t.speed(10)
+
+for i in range(30):
+    t.pencolor("red")
+    t.pensize(2)
+    t.left(29)
+
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+    t.forward(200)
+    t.left(90)
+t.done()
