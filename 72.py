2ba0cd59efaf5228cf52911c4eedf4bd17263892
Ansh Galani <85383708+AnshGalani@users.noreply.github.com>
2022-06-10 09:52:51 +0530

    Add files via upload

---
+++ 72.py
@@ -0,0 +1,30 @@
+from turtle import*
+import turtle
+
+shape("turtle")
+bgcolor("black")
+pencolor("gray");pensize(40);circle(180,360);rt(90);circle(180,360)
+rt(90);circle(180,360);rt(90);circle(180,360)
+
+pencolor("white");pensize(34);circle(160,360);rt(90);circle(160,360)
+rt(90);circle(160,360);rt(90);circle(160,360)
+
+pencolor("brown");pensize(28);circle(140,360);rt(90);circle(140,360)
+rt(90);circle(140,360);rt(90);circle(140,360)
+
+pencolor("red");pensize(25);circle(120,360);rt(90);circle(120,360)
+rt(90);circle(120,360);rt(90);circle(120,360)
+
+pencolor("yellow");pensize(15);circle(80,360);rt(90);circle(80,360)
+rt(90);circle(80,360);rt(90);circle(80,360)
+
+pencolor("aqua");pensize(10);circle(60,360);rt(90);circle(60,360)
+rt(90);circle(60,360);rt(90);circle(60,360)
+
+pencolor("red");pensize(5);circle(40,360);rt(90);circle(40,360)
+rt(90);circle(40,360);rt(90);circle(40,360)
+
+pencolor("white");pensize(2);circle(20,360);rt(90);circle(20,360)
+rt(90);circle(20,360);rt(90);circle(20,360)
+
+turtle.exitonclick()
