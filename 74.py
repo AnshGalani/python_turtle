c32c3dd6b2f04faaa1d09d7552a2e19bf5ca4082
Ansh Galani <85383708+AnshGalani@users.noreply.github.com>
2022-06-10 09:54:14 +0530

    Add files via upload

---
+++ 74.py
@@ -0,0 +1,16 @@
+from turtle import*
+import turtle
+
+shape("turtle")
+
+bgcolor("yellow");pencolor("blue");pensize(10);fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200)
+begin_fill();fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200);end_fill()
+fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200)
+begin_fill();fd(200);lt(90);fd(200);lt(90);fd(200);lt(90);fd(200);end_fill()
+
+begin_fill();pencolor("green");pensize(6);fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100);end_fill()
+fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100)
+begin_fill();fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100);end_fill()
+pencolor("green");fd(100);lt(90);fd(100);lt(90);fd(100);lt(90);fd(100)
+
+turtle.exitonclick()
